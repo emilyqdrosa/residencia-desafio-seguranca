88c40181947d6dbaa543cff3240e7de0457ddc6c
Emily Quevedo <emilyqdrosa@gmail.com>
2026-07-29 14:51:29 -0300

    add scoring constants

---
+++ src/ScoreCalculator.java
@@ -0,0 +1,11 @@
+public class ScoreCalculator {
+
+    private static final int REWARD_SCORE = 5;
+    private static final int URGENCY_SCORE = 10;
+    private static final int SUSPICIOUS_LINK_SCORE = 10;
+    private static final int FINANCIAL_SCORE = 15;
+    private static final int SENSITIVE_DATA_REQUEST_SCORE = 15;
+    private static final int INSTALL_REQUEST_SCORE = 15;
+
+
+}
