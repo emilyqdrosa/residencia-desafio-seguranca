0dacbb4fcf0991467f3d4f6c2eaf757d57c463a6
Emily Quevedo <emilyqdrosa@gmail.com>
2026-07-29 14:33:27 -0300

    add message receiving

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        System.out.println("=== VERIFICADOR DE GOLPE ===");
+        System.out.println("[ATENÇÃO] A verificação é um guia, não uma garantia absoluta de segurança.");
+        System.out.print("Insira a mensagem: ");
+        String message = sc.nextLine();
     }
 }
