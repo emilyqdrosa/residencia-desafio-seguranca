6e0f8df6a02ffb5faca7ba4d3d1fb55983b06f5d
Emily Quevedo <emilyqdrosa@gmail.com>
2026-07-29 16:18:04 -0300

    add scoreResultFunction, constructor and getScore

--- src/ScoreCalculator.java
+++ src/ScoreCalculator.java
@@ -7,5 +7,23 @@ public class ScoreCalculator {
     private static final int SENSITIVE_DATA_REQUEST_SCORE = 15;
     private static final int INSTALL_REQUEST_SCORE = 15;
 
+    private static int score;
 
+    public static void scoreResult(){
+        if (score <= 30){
+            System.out.println("[OK] Provavelmente legítima.");
+        } else if (score < 70){
+            System.out.println("[ATENÇÃO] Mensagem suspeita.");
+        } else {
+            System.out.println("[PERIGO] Provavelmente golpe.");
+        }
+    }
+
+    public ScoreCalculator() {
+        this.score = 0;
+    }
+
+    public int getScore() {
+        return score;
+    }
 }
